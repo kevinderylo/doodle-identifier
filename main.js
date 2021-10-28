2e844b9cf75b6f8eb2a1701efe9870c9a1fa229d
kevinderylo <76544058+kevinderylo@users.noreply.github.com>
2021-10-27 20:39:57 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(600, 600);
+    canvas.center();
+    background("white");
+}
+function draw(){
+
+}
+ function clearcanvas(){
+     background("white");
+ }
